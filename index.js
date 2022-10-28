92f6df3dd0920a5b8968d6462c5f1b43fe92b308
rahmanabdulquadir <rahmanaq777@gmail.comm>
2022-10-28 23:09:12 +0600

    updated

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
-const port  = process.env.PORT || 5000;
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-const courses = require('./data/courses.json')
-const course = require('./data/course.json')
+app.use(cors());
+const courses = require("./data/courses.json");
+const course = require("./data/course.json");
 
-app.get('/', (req, res) => {
-  res.send('courses API running')
+app.get("/", (req, res) => {
+  res.send("courses API running");
 });
 
-app.get('/courses', (req, res) => {
-  res.send(courses)
-})
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
 
-app.get('/course/:id', (req, res) => {
-  const id = req.params.id
-  const selectedCourse = course.find(c.id === id)
-  res.send(selectedCourse)
-})
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = course.find(c.id === id);
+  res.send(selectedCourse);
+});
 
 app.listen(port, () => {
-  console.log('courses server in running on', port)
-})
+  console.log("courses server in running on", port);
+});
