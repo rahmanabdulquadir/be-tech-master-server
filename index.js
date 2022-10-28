f01aa67d62c6102cb85f868224b2afb0dba70663
rahmanabdulquadir <rahmanaq777@gmail.comm>
2022-10-28 19:38:22 +0600

    courses added

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port  = process.env.PORT || 5000;
+
+app.use(cors())
+const courses = require('./data/courses.json')
+
+app.get('/', (req, res) => {
+  res.send('courses API running')
+});
+
+app.get('/courses', (req, res) => {
+  res.send(courses)
+})
+
+app.listen(port, () => {
+  console.log('courses server in running on', port)
+})
