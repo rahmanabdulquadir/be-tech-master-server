422e532ad030ad1d708cff788a19b807a5027baf
rahmanabdulquadir <rahmanaq777@gmail.comm>
2022-10-28 22:26:03 +0600

    photo added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port  = process.env.PORT || 5000;
 
 app.use(cors())
 const courses = require('./data/courses.json')
+const course = require('./data/course.json')
 
 app.get('/', (req, res) => {
   res.send('courses API running')
@@ -12,6 +13,12 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
   res.send(courses)
+})
+
+app.get('/course/:id', (req, res) => {
+  const id = req.params.id
+  const selectedCourse = course.find(c.id === id)
+  res.send(selectedCourse)
 })
 
 app.listen(port, () => {
